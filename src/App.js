b6c3f5f103b3ff38db239e4e519f4bd231f052da
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-06-10 11:18:29 -0600

    html fight

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [nemoSize, setNemoSize] = useState(20);
+  const [darlaSize, setDarlaSize] = useState(20);
+
+
+
+
+
+
+
+
   return (
     <div className="App">
-      <h1>Welcome to my App!</h1>
-      
+      <heaader>Welcome to my App!</heaader>
+      <div className="ocean">
+        <div className='fishie'>
+          <img src="darla2.png" width={darlaSize}></img>
+          <div className='button'>
+            <button onClick={() => setNemoSize(nemoSize + 10)}>Settle Bag</button>
+            <button onClick={() => setDarlaSize(nemoSize - 10)}>Shake Bag</button>
+          </div>
+        </div>
+        <div className='fishie'>
+          <img src="nemo1.png" width={nemoSize}></img>
+          <div className='button'>
+            <button onClick={() => setDarlaSize(darlaSize + 10)}>Shake Bag</button>
+            <button onClick={() => setNemoSize(darlaSize - 10)}>Get Caught</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
